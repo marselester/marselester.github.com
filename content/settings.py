10ca819743da1e6f166cf1e6f25a7089be0aaebb
marselester <marselester@ya.ru>
2012-11-08 19:22:32 +0600

    Added pelican settings

---
+++ content/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+AUTHOR = 'Marsel Mavletkulov'
+SITENAME = "marselester's blog"
+SITEURL = 'http://marselester.github.com'
+TIMEZONE = 'Asia/Yekaterinburg'
+
+GITHUB_URL = 'https://github.com/marselester/'
+DISQUS_SITENAME = 'marselester'
+TWITTER_USERNAME = 'marselester'
+
+SOCIAL = (
+    ('GitHub', 'https://github.com/marselester'),
+    ('Twitter', 'http://twitter.com/marselester'),
+    (u'Мой Круг', 'http://mmavletkulov.moikrug.ru/'),
+    ('LinkedIn', 'http://www.linkedin.com/in/marselester'),
+    ('Facebook', 'http://www.facebook.com/marselester'),
+)
