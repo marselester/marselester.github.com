64991382ce026f4dae7b0741d810cd5cd906c215
marselester <marselester@ya.ru>
2020-10-25 00:00:49 -0400

    Fix Instagram url

--- content/settings.py
+++ content/settings.py
@@ -13,7 +13,7 @@ TWITTER_USERNAME = 'marselester'
 
 SOCIAL = (
     ('GitHub', 'https://github.com/marselester'),
-    (u'Instagram 📷', 'instagram/index.html'),
+    (u'Instagram 📷', '{}/instagram/index.html'.format(SITEURL)),
     ('Twitter', 'http://twitter.com/marselester'),
     ('Medium', 'https://medium.com/@marselester/'),
 )
